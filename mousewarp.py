34f56acf441f7aaf311d2a430f2ca4f21a5f3c77
quartz_1216 <miyati388388@gmail.com>
2023-08-19 14:30:33 +0900

    add tasktray

--- mousewarp.py
+++ mousewarp.py
@@ -1,7 +1,19 @@
 import win32gui
 import win32api
 import win32con
+import pystray
 import time
+import threading
+from PIL import Image
+import sys
+from pathlib import Path
+
+def resource_path(relative_path):
+    base_path = getattr(sys, '_MEIPASS', str(Path(__file__).absolute().parent))
+    return str(Path(base_path).joinpath(relative_path))
+
+def on_exit(icon,item):
+    icon.stop()
 
 def get_active():
     active = win32gui.GetForegroundWindow()
@@ -16,15 +28,24 @@ def move_mouse(hwnd):
     except win32gui.error:
         pass
 
-active_hwnd = None
-hwnd = None
+def move_cursor_thread():
+    active_hwnd = None
+    while True:
+        time.sleep(0.1)
+        if win32api.GetAsyncKeyState(win32con.VK_MENU) & 0x8000:
+            while win32api.GetAsyncKeyState(win32con.VK_MENU) & 0x8000:
+                time.sleep(0.1)
+            hwnd = get_active()
+            if active_hwnd != hwnd: 
+                move_mouse(hwnd)
+                active_hwnd = get_active()
 
-while True:
-    time.sleep(0.1)
-    if win32api.GetAsyncKeyState(win32con.VK_MENU) & 0x8000:
-        while win32api.GetAsyncKeyState(win32con.VK_MENU) & 0x8000:
-            time.sleep(0.1)
-        hwnd = get_active()
-        if active_hwnd != hwnd: 
-            move_mouse(hwnd)
-            active_hwnd = get_active()
+image = Image.open(resource_path("icon.ico"))
+menu = (pystray.MenuItem("Exit", on_exit),) 
+icon = pystray.Icon("name", image, "MouseWarp", menu)
+
+cursor_thread = threading.Thread(target = move_cursor_thread)
+cursor_thread.daemon = True
+cursor_thread.start()
+
+icon.run()
